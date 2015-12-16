61df12307a3f160734998e86ca647bc9543f5315
aburhan <ameenah.burhan@gmail.com>
2015-12-15 22:11:46 -0500

    Using CrossPlatformInputManager.mp4

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	
+	}
+}
+ß
